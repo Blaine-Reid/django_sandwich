18584cf6b82617e7e72aed3e5d623cbad0fe10dc
Blaine-Reid <reid.blaine08@gmail.com>
2022-03-31 19:07:23 -0700

    'sandwich_app'

---
+++ sandwich_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from sandwich_app.views import IndexView, SandwichView, IngrediantView, RandomView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('sandwich/', SandwichView.as_view(), name='sandwich'),
+    path('sandwich/random/', RandomView.as_view(), name='random'),
+    path('sandwich/<str:ingrediant>', IngrediantView.as_view(), name='ingrediant'),
+]
---
+++ sandwich_app/views.py
@@ -0,0 +1,57 @@
+
+import random
+from django.shortcuts import render
+from django.views import View
+
+menu ={
+    'meats':['beef','chicken','turkey','pastrami'],
+    'cheeses':['jack','cheddar','swiss','pepper jack'],
+    'condiments':['mustard','mayo','vinegar'],
+    'veggies':['lettace','tomato','onion','pickles','olives'],
+    'breads':['white','rye','wheat','sourdough']
+}
+
+# Create your views here.
+class IndexView(View):
+    def get(self,request):
+    
+        return render(request=request, template_name='index.html')
+
+
+class SandwichView(View):
+    def get(self, request):
+        return render(
+            request=request, 
+            template_name='ingrediants.html',
+            context={'menu':menu.keys()}
+            )
+
+
+class IngrediantView(View):
+    def get(self, request, ingrediant):
+        if request.method == "GET":
+            return render(
+                request=request, 
+                template_name='ingrediant_list.html', 
+                context={
+                    'ingrediant':ingrediant,
+                    'ingrediants':menu[ingrediant]
+                    })
+
+
+class RandomView(View):
+    def get(self, request):
+        context={
+            'meat': random.choice(menu['meats']),
+            'cheese':random.choice(menu['cheeses']),
+            'condiment':random.choice(menu['condiments']),
+            'veggie':random.choice(menu['veggies']),
+            'bread':random.choice(menu['breads']),
+        }
+
+        if request.method == 'GET':
+            return render(
+                request=request, 
+                template_name='random.html', 
+                context= context
+            )
